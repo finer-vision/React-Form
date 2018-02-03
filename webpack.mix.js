d24d61534401735e8b28fa8bf9f419ff0ae9f489
James <jamescraig@finervision.com>
2018-02-03 18:19:08 +0000

    Refactored webpack config file.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,11 @@
 const mix = require('laravel-mix');
 
-mix.react('examples/basic/index.js', 'examples/build/basic.js');
-mix.react('examples/label/index.js', 'examples/build/label.js');
-mix.react('examples/group/index.js', 'examples/build/group.js');
+const examples = [
+    'basic',
+    'label',
+    'group'
+];
+
+examples.map(example => {
+    mix.react(`examples/${example}/index.js`, `examples/build/${example}.js`);
+});
