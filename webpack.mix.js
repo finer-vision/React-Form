99e0e38ed014b94fa7f598a649717f0bdb2252c1
James <jamescraig@finervision.com>
2018-02-03 18:16:56 +0000

    Added group example.

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,3 +2,4 @@ const mix = require('laravel-mix');
 
 mix.react('examples/basic/index.js', 'examples/build/basic.js');
 mix.react('examples/label/index.js', 'examples/build/label.js');
+mix.react('examples/group/index.js', 'examples/build/group.js');
